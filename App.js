51dba1cb62c63149cc6c959b5f4e9c8bdf7459c2
MacDevLC <carvingpixel@gmail.com>
2018-07-18 16:34:28 -0400

    base nav working model

--- App.js
+++ App.js
@@ -1,3 +1,38 @@
-import { FullNav } from './Navigation/navMain'
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-export default FullNav;
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Nav Homes</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Nav Settings!</Text>
+      </View>
+    );
+  }
+}
+
+
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
+});
+
+
+
